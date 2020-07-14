39f4539383ccbdc2dc9750571cd4dcac86a79892
Habib Yeo <habibyeo2@gmail.com>
2020-07-14 00:38:25 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+var car = {
+    'wheels': 'black',
+    'tires': 4,
+    'seats':['driver','passenger','back']
+}
+console.log(car.seats)
+
+
+var computer = {
+    'speakers': 4,
+    'color':'grey',
+    'apps': ['steam','chrome','vscode']
+}
+console.log(computer.apps)
+
+
+var phone = {
+    'speaker': 1,
+    'name': 'iphone',
+    'apps': ['instagram','youtube','soundcloud']
+}
+console.log(phone.name)
+
+
+function a () 
